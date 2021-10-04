df1c547541396e31dc8e55b4e82406756fa86dd9
LuizFrassi <luizfrassi@gmail.com>
2021-10-04 20:51:54 +0000

    Formataçao corrigida com lint

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, response, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '../../shared/errors/AppError';
@@ -11,19 +11,21 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-    if(error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            status: 'error',
-            message: error.message
-        });
-    };
-    return response.status(500).json({
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
         status: 'error',
-        message: error.message
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: error.message,
     });
-});
+  },
+);
 
-app.listen(9000,() => {
-    console.log('Server started on port 9000')
+app.listen(9000, () => {
+  console.log('Server started on port 9000');
 });
